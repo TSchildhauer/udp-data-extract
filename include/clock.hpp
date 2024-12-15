73d494965ab49fc2f24d2ed72c1391f04106d38b
Thomas Schildhauer <thomas.schildhauer@starship.co>
2024-12-15 10:40:33 +0200

    Autformat clock.hpp

--- include/clock.hpp
+++ include/clock.hpp
@@ -9,17 +9,21 @@ public:
     Clock()
         : syncPoint_(std::chrono::system_clock::now())
         , steadyRef_(std::chrono::steady_clock::now())
-    {}
+    {
+    }
 
-    auto steady_now() -> std::chrono::system_clock::time_point {
+    auto steady_now() -> std::chrono::system_clock::time_point
+    {
         const auto elapsed = std::chrono::steady_clock::now() - steadyRef_;
 
         return syncPoint_ + std::chrono::duration_cast<std::chrono::system_clock::duration>(elapsed);
     }
 
-    auto steady_now_ms() -> std::chrono::milliseconds {
+    auto steady_now_ms() -> std::chrono::milliseconds
+    {
         return std::chrono::duration_cast<std::chrono::milliseconds>(steady_now().time_since_epoch());
     }
+
 private:
     const std::chrono::system_clock::time_point syncPoint_;
     const std::chrono::steady_clock::time_point steadyRef_;
