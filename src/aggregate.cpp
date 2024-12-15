1819a63e513ecf906455d8223f23bcc1b3511825
Thomas Schildhauer <thomas.schildhauer@starship.co>
2024-12-15 19:23:50 +0200

    Build signal handling for async boost io and add csv style output for some graphs

--- src/aggregate.cpp
+++ src/aggregate.cpp
@@ -40,23 +40,23 @@ void Aggregate::schedule_next_spin()
     timer_.async_wait([this](boost::system::error_code ec) {
         if (!ec) {
             // Create the JSON object
-            // json::object output;
-            // output["timestamp"] = steady_synced_clock_.steady_now_ms().count();
+            json::object output;
+            output["timestamp"] = steady_synced_clock_.steady_now_ms().count();
 
             // std::string out = "t: " + std::to_string(steady_synced_clock_.steady_now_ms().count()) + ", ";
-            std::string out_csv = std::to_string(steady_synced_clock_.steady_now_ms().count());
+            // std::string out_csv = std::to_string(steady_synced_clock_.steady_now_ms().count());
 
             // extract_data destroys it, so this reference can't be const
             for (auto& c : clients_) {
+                output[c->get_name()] = c->get_last_data();
                 // out += c->get_name() + ": " + c->get_last_data() + ", ";
-                // output[c->get_name()] = c->get_last_data();
-                out_csv +=  ", " + c->get_last_data();
+                // out_csv +=  ", " + c->get_last_data();
             }
 
             // Print the JSON object as a line
+            std::cout << json::serialize(output) << std::endl;
             // std::cout << out << std::endl;
-            // std::cout << json::serialize(output) << std::endl;
-            std::cout << out_csv << std::endl;
+            // std::cout << out_csv << std::endl;
 
             next_execution_time_ += window_ms_;
             schedule_next_spin();
--- src/client1_main.cpp
+++ src/client1_main.cpp
@@ -8,10 +8,18 @@ int main()
     try {
         boost::asio::io_context io_context;
 
+        boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+        signals.async_wait([&](const boost::system::error_code& error, int signal_number) {
+            if (!error) {
+                std::cout << "Signal received: " << signal_number << std::endl;
+                io_context.stop(); // Stop the io_context to gracefully exit
+            }
+        });
+
         // Ports to connect to
         const std::vector<std::pair<uint16_t, std::string>> ports = { { 4001, "out1" }, { 4002, "out2" }, { 4003, "out3" } };
-        const std::string host{"127.0.0.1"};
-        const std::chrono::milliseconds window_ms{100};
+        const std::string host { "127.0.0.1" };
+        const std::chrono::milliseconds window_ms { 100 };
 
         Aggregate aggregate_client(io_context, window_ms, host, ports);
 
