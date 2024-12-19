259740eb0740f7f5201721f8ad2dff5a97325774
Thomas Schildhauer <thomas.schildhauer@starship.co>
2024-12-19 09:30:57 +0200

    Update control channel testing and try writing to some valid properties

--- tools/test_control_channel.py
+++ tools/test_control_channel.py
@@ -7,11 +7,30 @@ port = 4000 # control port UDP
 # Construct the binary message
 operation = 1  # read - 1, write - 2
 object_id = 1
-property_id = 0
+property_id = 1
+value = 10
 
-message = struct.pack(">HHH", operation, object_id, property_id)
-
-# Send the message
+# Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(message, (server_ip, port))
-print(f"Sent: {message.hex()}")
+# Set a timeout for receiving responses
+sock.settimeout(1)
+
+def send_message(object_id, property_id, operation = 1, value = None):
+    # Construct the message
+    if operation == 1:
+        message = struct.pack(">HHH", operation, object_id, property_id)
+    elif operation == 2:
+        message = struct.pack(">HHHH", operation, object_id, property_id, value)
+
+    # Send the message
+    sock.sendto(message, (server_ip, port))
+    # print(f"Sent: {message.hex()}")
+
+# Test lots of properties
+# for object_id in range(1,4):
+#     for property_id in range(1000):
+#         print(f"Testing object {object_id}, property {property_id}")
+#         send_message(object_id, property_id)
+
+# Write a value
+send_message(1, 170, 2, 8000)
